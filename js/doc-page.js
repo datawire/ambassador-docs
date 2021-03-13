f9ad20a234c4cfc028901ac9292f8e28c70f93f5
Brian Fleming <brianfleming@gmail.com>
2021-03-13 12:41:47 -0500

    (from getambassador.io) add new docs questions component to have some consistency on all pages

--- js/doc-page.js
+++ js/doc-page.js
@@ -6,6 +6,7 @@ import { MDXRenderer } from 'gatsby-plugin-mdx';
 import Layout from '../../../../src/components/Layout';
 import Sidebar from './Sidebar';
 import DocFooter from '../../../../src/components/DocFooter';
+import DocQuestions from '../../../../src/components/DocQuestions/DocQuestions';
 
 import Link from '../../../../src/components/Link';
 
@@ -233,14 +234,7 @@ export default ({ data, location }) => {
             <MDXRenderer slug={page.fields.slug}>
               {template(page.body, versions)}
             </MDXRenderer>
-            <div>
-              <h2>Questions?</h2>
-              <p>
-                We’re here to help. If you have questions,{' '}
-                <a href="http://d6e.co/slack">join our Slack</a>{' '}or{' '}
-                <a href="/contact-us/">contact us</a>.
-              </p>
-            </div>
+            <DocQuestions />
           </main>
         </div>
         <DocFooter page={page} branch="1.11" />
