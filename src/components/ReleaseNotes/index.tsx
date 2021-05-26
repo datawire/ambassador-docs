9494d88a8b8a130c41b60ff1dce35b9dfb27793f
Julian Arana <julianarana@gmail.com>
2021-05-26 16:12:43 -0500

    (from getambassador.io) Release Notes (#840)
    * Read the contents of the JSON files from the release notes on the documents
    
    * Release Notes Layout with the information from the Json
    
    * Add icons
    
    * Include pages for all the projects
    
    * Include pages for all the projects
    
    * Infer pages releseNotes.json file
    
    * Adjust engine to read yaml files instead of json files
    
    * Display images on folder
    
    * Remove uneeded files
    
    * Support for mobile versions
    
    * Remove Telepresence 2.1 release notes
    
    * Include Edge stack Release notes
    
    * Add argo, cloud, edge-stack dates
    
    * Update argo, telepresence and edgeStack documentation
    
    * Comments on PR
    
    * Remove link buttons
    
    * Comments on the PR
    
    * Include documentation for the release notes process, add examples and remove cloud and argo
    
    * PR Comments
    
    * Change edge-stack screenshot
    
    * Tweaking wording.
    
    * Fixing typos.
    
    * Add title according to the section, add changelog link and correct dates problem
    
    * Tweaking wording.
    
    * Correcting paths.
    
    * Make image clickable to enlarge and replace old images
    
    * Remove bogus text.
    
    * Comments on PR
    
    * Correct Meta tags
    
    * Meta tags for cloud and argo
    
    Co-authored-by: Kenn Hussey <kennhussey@datawire.io>

---
+++ src/components/ReleaseNotes/index.tsx
@@ -0,0 +1,62 @@
+import React, { useMemo } from 'react';
+import { Link } from 'gatsby';
+import Release from './Release';
+
+const getChangeLogUrl = (product) => {
+  switch (product) {
+    case 'edge-stack':
+      return 'https://github.com/telepresenceio/telepresence/blob/release/v2/CHANGELOG.md';
+    case 'telepresence':
+      return 'https://github.com/telepresenceio/telepresence/blob/release/v2/CHANGELOG.md';
+    case 'argo':
+    case 'cloud':
+    default:
+      return null;
+  }
+};
+
+const ReleaseNotes = ({ releases, images, product }) => {
+  const title = useMemo(() => {
+    switch (product) {
+      case 'edge-stack':
+        return 'Edge Stack Release Notes';
+      case 'telepresence':
+        return 'Telepresence Release Notes';
+      case 'argo':
+        return 'Argo Release Notes';
+      case 'cloud':
+        return 'Cloud Release Notes';
+      default:
+        return 'Release Notes';
+    }
+  }, [product]);
+
+  const changelog = useMemo(() => {
+    const commonText = `For a detailed list of all the changes in these releases, please consult the`;
+    const url = getChangeLogUrl(product);
+
+    if (url) {
+      return (
+        <>
+          {commonText} <Link to={url}>CHANGELOG.</Link>
+        </>
+      );
+    }
+
+    return null;
+  }, [product]);
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{changelog}</p>
+      <div>
+        {releases.map((release) => (
+          <Release key={release.version} release={release} images={images} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default ReleaseNotes;
