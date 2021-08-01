a380870ae5856ffca264e98eaa2332e6f8df76db
Luke Shumaker <lukeshu@datawire.io>
2021-08-01 10:21:47 -0600

    (from getambassador.io) Replace gatsby-remark-autolink-headers with our own version
    Our version emits almost exactly the same HTML (I got rid of the inline
    style and moved it to the stylesheet).  However, ours is implemented
    as a component so that it plays nice with the $variable$ expansion.

---
+++ src/components/Heading/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import Link from '../../../../src/components/Link';
+import Icon from './icon.inline.svg';
+
+// heading(n) takes an integer n in the range 1-6, and returns a React
+// component for a heading of that level.
+export const heading = (n) => {
+  const Tag = `h${n}`;
+
+  // Return a classical component rather than a functional component
+  // because when a functional component is nested inside of another
+  // function (as this is) various things (including ESLint and
+  // sometimes React itself?) get confused in to thinking that it's a
+  // hook/callback and not a component, and then error at you about
+  // having broken the "Rules of Hooks"[1].
+  //
+  // [1]: https://reactjs.org/docs/hooks-rules.html
+  class Heading extends React.Component {
+    render() {
+      let { children, ...props } = this.props;
+
+      if (!props.id) {
+        return <Tag {...props}>{children}</Tag>;
+      }
+
+      return (
+        <Tag {...props}>
+          <Link
+            to={'#' + props.id}
+            aria-label={props.id.split('-').join(' ')}
+            className="anchor before"
+          >
+            <Icon />
+          </Link>
+          {children}
+        </Tag>
+      );
+    }
+  }
+
+  return Heading;
+};
