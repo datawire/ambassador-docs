4a5b1203c5ac7968514c92cdd036e1406185bb01
Alex Ocampo <joseocampo-gomez@datawire.io>
2022-02-10 11:02:49 -0600

    (from getambassador.io) remove useless code

--- src/products/Telepresence.js
+++ src/products/Telepresence.js
@@ -1,15 +1,9 @@
-import { Link } from 'gatsby';
 import React from 'react';
 
 import CodeHero from '../../../src/assets/images/code/code_Hero.svg';
 import CodeDocTele from '../../../src/assets/images/code/code_doc_tele.svg';
 import CodeQuickStart from '../../../src/assets/images/code/code_quick_start_hero.svg';
 import CodeTele from '../../../src/assets/images/code/code_tele.svg';
-//import Card from '../../../src/components/Card';
-import Icon from '../../../src/components/Icon';
-import { goToTelepresenceQuickStart } from '../../../src/utils/routes';
-
-import { TelepresenceCards } from './config';
 
 const Card = ({ img, title, description }) => (
   <div className="docs__quickStart-card">
@@ -70,35 +64,5 @@ const Telepresence = () => {
     </section>
   );
 };
-
-const Telepresence1 = () => (
-  <section className="docs__started docs__container product-page">
-    <h1 className="docs__heading-primary docs__heading-primary--aligned">
-      <Icon name="telepresence-icon" /> Telepresence
-    </h1>
-    <p>
-      Code and test microservices locally against a remote Kubernetes cluster.
-    </p>
-    <Link
-      to={goToTelepresenceQuickStart}
-      className="docs__button-secondary docs__m-bottom-50"
-    >
-      Get Started{' '}
-      <Icon name="right-arrow" className="docs__button-secondary--arrow" />
-    </Link>
-    <div className="docs__cards">
-      {TelepresenceCards.map((c) => (
-        <Card
-          key={c.title}
-          title={c.title}
-          description={c.description}
-          link={c.link}
-          linkText={c.linkText}
-          icon={c.icon}
-        />
-      ))}
-    </div>
-  </section>
-);
 
 export default Telepresence;
