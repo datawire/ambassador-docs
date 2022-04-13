41f5f1e5507b1dd84f92efc2e52917d5915404de
Leonel Contreras <lcontreras@datawire.io>
2022-04-13 16:03:54 -0600

    (from getambassador.io) Searchbar on mobile devices

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -4,24 +4,30 @@ import { Helmet } from 'react-helmet';
 import SearchIcon from './search.inline.svg';
 
 export default function SearchBox(props) {
-  const loadJS = () => {
-    // Might fail if window.docsearch isn't set yet or if the DOM
-    // hasn't been updated yet to include the #doc-search element.
-    // If that happens, just back off and try again.
-    try {
-      window.docsearch({
-        apiKey: '8f887d5b28fbb0aeb4b98fd3c4350cbd',
-        indexName: 'getambassador',
-        inputSelector: `#${inputSelector}`,
-        debug: true,
-      });
-    } catch {
-      setTimeout(loadJS, 500);
-    }
-  };
-  
   const inputSelector = props.selector || "doc-search";
-  loadJS();
+
+  const isMobile = React.useMemo(() => {
+    return typeof window !== 'undefined' ? window.innerWidth <= 800 : true;
+  }, []);
+
+  React.useEffect(() => {
+    const loadJS = () => {
+      // Might fail if window.docsearch isn't set yet or if the DOM
+      // hasn't been updated yet to include the #doc-search element.
+      // If that happens, just back off and try again.
+      try {
+        window.docsearch({
+          apiKey: '8f887d5b28fbb0aeb4b98fd3c4350cbd',
+          indexName: 'getambassador',
+          inputSelector: `#${inputSelector}`,
+          debug: true,
+        });
+      } catch {
+        setTimeout(loadJS, 500);
+      }
+    };
+    loadJS();
+  }, [isMobile]);
 
   return (
     <div className="docs__search-box">
