b70ae2f423a6dea70294ace6c15a17483b922460
DromeroUnosquare <84340465+DromeroUnosquare@users.noreply.github.com>
2022-05-12 21:20:04 -0500

    (from getambassador.io) Create a util to use getSpecialTitles function

--- src/index.js
+++ src/index.js
@@ -44,6 +44,7 @@ import './style.less';
 import getDocsActiveVersion from './utils/getDocsActiveVersion';
 import getPrevNext from './utils/getPrevNext';
 import { canonicalUrls } from './utils/getValueCanonicals';
+import getSpecialTitles from './utils/getSpecialTitles'
 
 const index = ({ data, location, pageContext }) => {
   const page = data.mdx || {};
@@ -753,60 +754,6 @@ const index = ({ data, location, pageContext }) => {
     }
   }
 
-  const fixTitleProduct = (data) => {
-    const edgeStackTitle = data.replace('| Ambassador', '| Edge Stack');
-    const telepresenceTitle = data.concat(' Telepresence');
-    const argoTitle = data.concat(' Argo');
-    const cloudTitle1 = data.replace(
-      'Quick start',
-      'Visualize API - Quick start',
-    );
-    const cloudTitle2 = data.replace(
-      'Quick start',
-      'Service Catalog - Quick start',
-    );
-    const specialTitleEmissary = data.replace(
-      'The Ambassador operating model:',
-      'Operating model:',
-    );
-    const specialTitleEdgeStack = data.replace(
-      'The Ambassador operating model: GitOps and continuous delivery | Ambassador',
-      'Operating model: GitOps and continuous delivery | Edge Stack',
-    );
-
-    if (
-      canonicalUrl.includes('edge-stack') &&
-      canonicalUrl.includes('/concepts/gitops-continuous-delivery/')
-    ) {
-      return specialTitleEdgeStack;
-    }
-    if (canonicalUrl.includes('edge-stack')) {
-      return edgeStackTitle;
-    }
-    if (canonicalUrl.includes('telepresence')) {
-      return telepresenceTitle;
-    }
-    if (canonicalUrl.includes('argo')) {
-      return argoTitle;
-    }
-    if (
-      canonicalUrl.includes('cloud') &&
-      canonicalUrl.includes('visualize-api')
-    ) {
-      return cloudTitle1;
-    }
-    if (
-      canonicalUrl.includes('cloud') &&
-      canonicalUrl.includes('service-catalog')
-    ) {
-      return cloudTitle2;
-    }
-    if (canonicalUrl.includes('/concepts/gitops-continuous-delivery/')) {
-      return specialTitleEmissary;
-    }
-    return data;
-  };
-
   return (
     <Layout
       location={location}
@@ -814,7 +761,7 @@ const index = ({ data, location, pageContext }) => {
       customAnnouncementClass="docs-announcement-container"
     >
       <SEO
-        title={fixTitleProduct(metadata.metaTitle)}
+        title={getSpecialTitles(metadata.metaTitle, canonicalUrl)}
         type="article"
         canonicalUrl={canonicalUrl}
         description={metadata.metaDescription}
