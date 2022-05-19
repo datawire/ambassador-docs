d8e42d627b88df0fa216b832d3b4416d6fb0b447
aaronUnosquare <88786786+aaronUnosquare@users.noreply.github.com>
2022-05-19 14:57:37 -0500

    (from getambassador.io) Use relative path and add validation for links that start with "." or ".."

--- docs/ship/latest/quick-start/ArgoQuickStartLanding.js
+++ docs/ship/latest/quick-start/ArgoQuickStartLanding.js
@@ -40,7 +40,7 @@ class ArgoQuickStartLanding extends Component {
             </ol>
             <GatsbyLink
               id="tp-demo-option-a"
-              to="/docs/argo/latest/quick-start/new-to-kubernetes"
+              to="./new-to-kubernetes"
               className="get-started-button"
             >
               Get Started
@@ -63,16 +63,16 @@ class ArgoQuickStartLanding extends Component {
               <li>Set up your service and image repositories</li>
               <li>Define and run a Canary Rollout on your service</li>
             </ol>
-            <a
+            <GatsbyLink
               id="tp-intercepts-option-b"
-              href="/docs/argo/latest/howtos/configure-argo-rollouts/"
+              to="../../../argo/latest/howtos/configure-argo-rollouts/"
               className="get-started-button"
             >
               Get Started
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path d="M13.3579 4.4545C12.9186 4.01517 12.2063 4.01517 11.7669 4.4545C11.3276 4.89384 11.3276 5.60615 11.7669 6.04549L16.5969 10.8755H4.68768C4.06636 10.8755 3.56268 11.3792 3.56268 12.0005C3.56268 12.6218 4.06636 13.1255 4.68768 13.1255H16.596L11.7669 17.9545C11.3276 18.3938 11.3276 19.1061 11.7669 19.5455C12.2063 19.9848 12.9186 19.9848 13.3579 19.5455L20.1079 12.7955C20.5473 12.3562 20.5473 11.6438 20.1079 11.2045L13.3579 4.4545Z" />
               </svg>
-            </a>
+            </GatsbyLink>
           </div>
         </div>
       </div>
