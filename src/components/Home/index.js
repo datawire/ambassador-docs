893571b44533a19b2ec63bc2d4056b12cfec8d23
Alex Ocampo <jose.ocampo.gomez@gmail.com>
2022-09-01 17:24:57 -0500

    (from getambassador.io) more SEO titles

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -43,6 +43,7 @@ import SidebarContent from '../SidebarContent';
 
 const index = ({ data, location, pageContext }) => {
   const page = data.mdx || {};
+  console.log('AQUI=>12');
   const slug = page.fields.slug.split('/');
   const isHome = page.fields.slug === '/docs/';
   const initialProduct = isHome
