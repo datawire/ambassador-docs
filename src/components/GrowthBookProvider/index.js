69ebeebbf15086d656a378d648ffeb0475b2cdf8
Alex Ocampo <joseocampo-gomez@datawire.io>
2022-09-27 17:29:49 -0500

    (from getambassador.io) add logs to test

--- src/components/GrowthBookProvider/index.js
+++ src/components/GrowthBookProvider/index.js
@@ -27,6 +27,7 @@ const GrowthBookProviderWrapper = ({ userId, children }) => {
   const dispatch = useAppDispatch();
   const DAYS_TO_EXPIRE = 30;
   const fetchFeatures = () => {
+    console.log('Here fetchFeatures', process.env.GATSBY_GROWTHBOOK_URL);
     fetch(process.env.GATSBY_GROWTHBOOK_URL)
       .then((res) => res.json())
       .then((json) => {
@@ -74,6 +75,7 @@ const GrowthBookProviderWrapper = ({ userId, children }) => {
 
   useEffect(() => {
     const isValidPath = allowedPathNames.includes(pathname);
+    console.log('Here useEffect', isValidPath, pathname);
     if (isValidPath) {
       fetchFeatures();
     } else {
