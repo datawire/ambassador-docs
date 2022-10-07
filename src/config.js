63b8996e7ac8693721ab79d130e752cd78cba49e
Leonel Contreras <lcontreras@datawire.io>
2022-10-07 14:43:24 -0500

    (from getambassador.io) Remove the 1.0 from build, remove Markdown component

--- src/config.js
+++ src/config.js
@@ -29,7 +29,6 @@ export const products = [
         link: 'pre-release/quick-start',
       },
       { id: 'latest', name: 'Latest', link: 'latest/quick-start' },
-      { id: '1.0', name: '1.0', link: '1.0/discussion/overview' },
       { id: '2.7', name: '2.7', link: '2.7/quick-start' },
       { id: '2.6', name: '2.6', link: '2.6/quick-start' },
       { id: '2.5', name: '2.5', link: '2.5/quick-start' },
