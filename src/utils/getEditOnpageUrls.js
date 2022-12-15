2e7c440eba5019095151dbab2226865bfcbecc3e
Leonel Contreras <lcontreras@datawire.io>
2022-12-15 12:14:24 -0600

    (from getambassador.io) Fix edit this page BLS

---
+++ src/utils/getEditOnpageUrls.js
@@ -0,0 +1,9 @@
+export default {
+  '/docs/emissary/pre-release/topics/using/headers/add_request_headers/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/using/headers/add-request-headers.md',
+  '/docs/emissary/pre-release/topics/using/headers/add_response_headers/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/using/headers/add-response-headers.md',
+  '/docs/emissary/pre-release/topics/using/headers/remove_request_headers/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/using/headers/remove-request-headers.md',
+  '/docs/emissary/pre-release/topics/using/headers/remove_response_headers/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/using/headers/remove-response-headers.md',
+  '/docs/emissary/pre-release/topics/running/services/ext_authz/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/running/services/ext-authz.md',
+  '/docs/emissary/pre-release/topics/using/query_parameters/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/using/query-parameters.md',
+  '/docs/emissary/pre-release/topics/using/prefix_regex/': 'https://github.com/datawire/ambassador-docs/blob/master/docs/emissary/3.3/topics/using/prefix-regex.md',
+};
