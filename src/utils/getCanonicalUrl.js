abefd86a19a22091ea465499ba86d3bcab81d0b7
Alex Ocampo <joseocampo-gomez@datawire.io>
2023-01-05 17:37:13 -0600

    (from getambassador.io) change canonical url

--- src/utils/getCanonicalUrl.js
+++ src/utils/getCanonicalUrl.js
@@ -6,6 +6,8 @@ const { trimLastSlash } = require('../../../src/utils/trimUrl');
 const exceptions = {
   '/docs/cloud/service-catalog/howtos/complete-k8s/':
     '/docs/telepresence/latest/service-catalog/howtos/complete-k8s/',
+  '/docs/edge-stack/latest/topics/install/upgrade/helm/edge-stack-2.5/edge-stack-3.X':
+    '/docs/edge-stack/latest/topics/install/upgrade/helm/edge-stack-3.3/edge-stack-3.X ',
 };
 
 const getCanonicalUrl = (slugs, currentSlug) => {
