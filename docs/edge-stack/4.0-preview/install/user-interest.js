115bec9c96ad5b8a226d086c0b76e3635ae1adf8
Lance Austin <laustin@datawire.io>
2023-07-20 13:07:49 -0500

    (from getambassador.io) aes4: address feedback on feedback form
    - changed title
    - added padding
    - auto-close on submit after 5 seconds
    - force user to fill out form
    - allow user to click close button or background once successfully submitted
    
    Signed-off-by: Lance Austin <laustin@datawire.io>

--- docs/edge-stack/4.0-preview/install/user-interest.js
+++ docs/edge-stack/4.0-preview/install/user-interest.js
@@ -29,36 +29,47 @@ const styles = (theme) => ({
 });
 
 const DialogTitle = withStyles(styles)((props) => {
-  const { children, classes, onClose, ...other } = props;
+  const { children, classes, onClose, isSubmitted, ...other } = props;
+
+  const renderCloseButton = () => {
+    if (isSubmitted) {
+      return (
+        <IconButton
+          aria-label="close"
+          className={classes.closeButton}
+          onClick={onClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      );
+    }
+  };
+
   return (
     <MuiDialogTitle disableTypography className={classes.root} {...other}>
       <Typography variant="h6" className={classes.titleOverride}>
         {children}
       </Typography>
-      <IconButton
-        aria-label="close"
-        className={classes.closeButton}
-        onClick={onClose}
-      >
-        <CloseIcon />
-      </IconButton>
+      {renderCloseButton()}
     </MuiDialogTitle>
   );
 });
 
 const DialogContent = withStyles((theme) => ({
   root: {
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
   },
 }))(MuiDialogContent);
 
 // UserInterestForm is a wrapper component around the HubspotForm component which prompts a user to provide contact information that can be used for future marketing campaigns.
 const UserInterestForm = () => {
-  const key = 'ambassador-aes4';
+  const storageKey = 'ambassador-aes4';
+
+  let [isSubmitted, setIsSubmitted] = useState(false);
 
   let [isVisible, setIsVisible] = useState(() => {
     if (isBrowser) {
-      const saved = localStorage.getItem(key);
+      const saved = localStorage.getItem(storageKey);
       const initialValue = JSON.parse(saved);
       return initialValue ? initialValue.isContactFormVisible : true;
     }
@@ -69,16 +80,25 @@ const UserInterestForm = () => {
 
   useEffect(() => {
     const data = JSON.stringify({ isContactFormVisible: isVisible });
-    localStorage.setItem('ambassador-aes4', data);
+    localStorage.setItem(storageKey, data);
   }, [isVisible]);
 
   const handleCloseForm = (e, reason) => {
-    if (reason == 'backdropClick') {
+    if (reason == 'backdropClick' && !isSubmitted) {
       e.preventDefault();
       return;
     }
 
     setIsVisible(false);
+  };
+
+  const handleOnSubmit = () => {
+    setIsSubmitted(true);
+
+    // auto-close after 5 seconds if users hasn't already closed
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 5000);
   };
 
   return (
@@ -90,15 +110,19 @@ const UserInterestForm = () => {
         fullWidth={true}
         maxWidth={'md'}
       >
-        <DialogTitle id="customized-dialog-title" onClose={handleCloseForm}>
-          Get notified of Edge Stack 4 Preview updates
+        <DialogTitle
+          id="customized-dialog-title"
+          onClose={handleCloseForm}
+          isSubmitted={isSubmitted}
+        >
+          {isSubmitted ? 'Thank you...' : 'Join Edge Stack 4 Preview'}
         </DialogTitle>
         <DialogContent dividers>
           <HubspotForm
             portalId="485087"
             formId="12a65b2b-9aef-49ff-8ded-bf9fda0faf10"
-            onSubmit={() => {}}
-            loading={<div>Loading...</div>}
+            onSubmit={handleOnSubmit}
+            loading={<div>Loading signup form...</div>}
           />
         </DialogContent>
       </Dialog>
